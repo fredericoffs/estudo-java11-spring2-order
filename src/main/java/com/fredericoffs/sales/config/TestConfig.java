d4c95528e8ee62d48ca34aa016c59b6aa769376c
Frederico F Silva <fredericoffs@live.com>
2021-05-20 17:53:30 -0300

    Payment, one-to-one association

--- src/main/java/com/fredericoffs/sales/config/TestConfig.java
+++ src/main/java/com/fredericoffs/sales/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.fredericoffs.sales.entities.Category;
 import com.fredericoffs.sales.entities.Order;
 import com.fredericoffs.sales.entities.OrderItem;
+import com.fredericoffs.sales.entities.Payment;
 import com.fredericoffs.sales.entities.Product;
 import com.fredericoffs.sales.entities.User;
 import com.fredericoffs.sales.entities.enums.OrderStatus;
@@ -54,9 +55,9 @@ public class TestConfig implements CommandLineRunner {
 		userRepositoy.saveAll(Arrays.asList(u1, u2));
 
 		// string com padrão de horário UTC "2019-06-20T19:53:07Z"
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.SHIPPED, u1);
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.CANCELED, u2);
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.PAID, u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
 		orderRepositoy.saveAll(Arrays.asList(o1, o2, o3));
 
@@ -90,6 +91,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepositoy.save(o1);
 
 	}
 
